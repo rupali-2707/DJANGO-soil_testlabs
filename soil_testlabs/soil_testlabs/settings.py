84ba768dc2118488d49adec09242d1d46e8b4936
Rupali <60186830+rupali-2707@users.noreply.github.com>
2020-09-01 03:54:33 +0530

    Update settings.py

--- soil_testlabs/soil_testlabs/settings.py
+++ soil_testlabs/soil_testlabs/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'soil_testlabs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
